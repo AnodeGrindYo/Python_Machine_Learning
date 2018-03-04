6bcd00820f41fdec348dd4ad6f410e78cf246faa
adriengodoy <mr.a.godoy@gmail.com>
2018-03-04 17:16:45 +0100

    Gestion des données manquantes

--- part_1_Data_preprocessing/data_preprocessing.py
+++ part_1_Data_preprocessing/data_preprocessing.py
@@ -14,5 +14,12 @@ dataset = pd.read_csv('Data.csv') # permet d'importer les datas du csv
 # Création de la matrice des variables indépendantes
 # ':' toutes les lignes du dataset
 #☺ ':-1' toutes les colones sauf la dernière
-X = dataset.iloc[:, :-1].values # récupération des indices du dataset
-Y = dataset.iloc[:, -1].values
+x = dataset.iloc[:, :-1].values # récupération des indices du dataset
+y = dataset.iloc[:, -1].values
+
+# Gérer données manquantes
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+#☻ associe l'imputer à x et remplace les dnnées manquantes par la moyenne des colonnes
+imputer.fit(x[:, 1:3]) # range de 1 à 3 donc prends les colonnes 1 et 2
+x[:, 1:3] = imputer.transform(x[:, 1:3])
